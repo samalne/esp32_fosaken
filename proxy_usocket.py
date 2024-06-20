a7c149171f52b1f0c761a10d55c2706c967e278b
AlSamh AlAlwani <s_a_m_h@outlook.com>
2024-06-20 20:53:56 +0300

    Added boot file, added hexdump function to proxy.py

--- proxy_usocket.py
+++ proxy_usocket.py
@@ -9,12 +9,22 @@ import _thread
                                                                         
 
 
-
-HEX_FILTER = ''.join(
-    (len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
+# This is the dumping of hex values and represented chars using a dict filter
+HEX_FILTER = ''.join([(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
 
 def hexdump(src, length=16, show=True):
     if isinstance(src, bytes):
         src.decode()
     results = list()
-    for i in range(0, len(src), length)
+    for i in range(0, len(src), length):
+        word = str(src[i:i+length])
+
+        printable = word.translate(HEX_FILTER)
+        hexa = ''.join([f'{ord(c):02X}' for c in word])
+        hexwidth = length*3
+        results.append(f'{i:04x} {hexa:<{hexwidth}} {printable}')
+    if show:
+        for line in results:
+            print(line)
+        else:
+            return results
