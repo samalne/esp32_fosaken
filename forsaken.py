085de91a2316be415e15f27f4c46a3a419ad7d57
AlSamh AlAlwani <s_a_m_h@outlook.com>
2024-06-20 03:35:34 +0300

    Added some functionality, made some comments

--- forsaken.py
+++ forsaken.py
@@ -1,12 +1,21 @@
-from machine import Pin, PWM, SoftI2C
+from machine import Pin, PWM, UART, SoftI2C, SoftSPI
 import time
 import ssd1306
+import network
+import os, vfs
 
 # TOADD: start the on boot code
 
 # init code to start loading the I2C for the screen, buttons
 # buzzer pin, and extra native device programable pins
- 
+
+#   ___ _   _ ___ _____ 
+# |_ _| \ | |_ _|_   _|
+#  | ||  \| || |  | |  
+#  | || |\  || |  | |  
+# |___|_| \_|___| |_|  
+                     
+
 # def init(): for the initialization of oled, buttons, buzzer, gpio as dependand
 def init():
     # button init as input
@@ -26,19 +35,72 @@ def init():
     buzz = PWM(buz_pin, freq=2000, duty=20)
     buzz.duty(0)
 
+# __        _____ _____ ___ 
+# \ \      / /_ _|  ___|_ _|
+#  \ \ /\ / / | || |_   | | 
+#   \ V  V /  | ||  _|  | | 
+#    \_/\_/  |___|_|   |___|
+                          
 
 
-# def wifi_con(ssid, pass):
-#     sta = WLAN
+# This would initialize the wifi object 
+def wifi_config():
+    wlan = network.WLAN(netowrk.STA_IF)
+    wlan.ative(True)
+    wlan.scan()
+    wlan.isconnected()
+    # wlan.connect('ssid', 'pass')
+    # wlan.config('mac')
+    # wlan.ifconfig()
+
+# For creation of an access point 
+def create_ap(wifi_ssid, wifi_pass, cn):
+    ap = network.WLAN(netowrk.STA_IF)
+    ap.config(ssid=wifi_ssid)
+    ap.config(max_clients=cn)
+    ap.active()
+
+
+# To connect to a given network ssid, with key as password
+def do_connect(ssid, key):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(ssid, key)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+#  _        _    _   _ 
+# | |      / \  | \ | |
+# | |     / _ \ |  \| |
+# | |___ / ___ \| |\  |
+# |_____/_/   \_\_| \_|
+   
+## This is for future implementation ex: SPI ethernet controller
+# def lan_config():
+#     lan = network.LAN(mdc=PIN_MDC,  )
+#     lan.active()
+#     lan.ifconfig()
 # load the screen/welcoming, or logo for the oled display
 
 # some simple audio melody to be played
+# need to import audio code library but not sure due to large sizes
 
-# maybe some audio decoding
+#  ____  ____   ____              _ 
+# / ___||  _ \ / ___|__ _ _ __ __| |
+# \___ \| | | | |   / _` | '__/ _` |
+#  ___) | |_| | |__| (_| | | | (_| |
+# |____/|____/ \____\__,_|_|  \__,_|
+
+
+# def init_sdcard():
+#     sd = machine.SDCard(slot=2)
+#     vfs.mount(sd, '/sd')
+#     os.listdir('/sd')
+#     vfs.unmount('/sd')
 
 # options to chose the mode (desktop/hax gadget)
 
-# connect / hak into a wifi (require a hacking module/wifi manager)
-# Wifi manager
 
-# sd card implementation
